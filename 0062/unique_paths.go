5a1d3018aefa1c4a0941e959a9fe7dcc52b6e56d
samwelkey <wenju.gao124@gmail.com>
2019-05-09 21:48:03 +0800

    Add 00062

---
+++ 0062/unique_paths.go
@@ -0,0 +1,16 @@
+func uniquePaths(m int, n int) int {
+    matrix := make([][]int,m)
+    for i:=0;i<m;i++{
+        matrix[i] = make([]int,n)
+    }
+    for i:=0;i< m;i++{
+        for j:=0;j<n;j++{
+            if i==0 || j==0{
+                matrix[i][j] = 1
+            }else{
+                matrix[i][j]=matrix[i-1][j]+matrix[i][j-1]
+            }
+        }
+    }
+    return matrix[m-1][n-1]
+}
